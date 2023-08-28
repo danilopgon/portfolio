ba43089364994b77543ac95bc9bbded63f6a867a
danilopgon <dani.lopezgonzalez.94@gmail.com>
2023-08-28 13:11:50 +0200

    Add prettier

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,22 +1,20 @@
-import { Card, CardFooter } from "@nextui-org/card";
-import { Image } from "@nextui-org/image";
-import { Button } from "@nextui-org/button";
-import Link from "next/link";
+import { Card, CardFooter } from '@nextui-org/card'
+import { Image } from '@nextui-org/image'
+import { Button } from '@nextui-org/button'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center align-middle p-24">
-      <div className="w-full lg:w-2/3">
+    <main className="flex h-screen flex-col items-center justify-center p-24 align-middle">
+      <div className=" container m-3 w-full bg-blue-50 p-2 lg:w-2/3 ">
         <Card isFooterBlurred radius="lg" className="border-none">
           <Image
             alt="danilopgon"
-            className="object-cover w-full"
+            className="w-full object-cover"
             src="https://danilopgon.com/wp-content/uploads/2020/09/encabezado-retocado-definitivo-2-arreglado.jpg"
           />
-          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-            <p className="text-tiny text-white/80">
-              danilopgon's portfolio template
-            </p>
+          <CardFooter className="absolute bottom-1 z-10 ml-1 w-[calc(100%_-_8px)] justify-between overflow-hidden rounded-large border-1 border-white/20 py-1 shadow-small before:rounded-xl before:bg-white/10">
+            <p className="text-tiny text-white/80">danilopgon's portfolio template</p>
             <Link href="https://github.com/danilopgon/portfolio-2023">
               <Button>Repository</Button>
             </Link>
@@ -24,5 +22,5 @@ export default function Home() {
         </Card>
       </div>
     </main>
-  );
+  )
 }
