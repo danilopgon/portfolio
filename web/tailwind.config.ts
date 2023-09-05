d97bbb91b94ec8a10f38356621b968d256db9b09
danilopgon <dani.lopezgonzalez.94@gmail.com>
2023-09-05 17:59:17 +0200

    Update tailwind config

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,40 +1,80 @@
 import type { Config } from 'tailwindcss'
-import {nextui} from "@nextui-org/react";
+import { nextui } from '@nextui-org/react'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [
     nextui({
-      prefix: "nextui", // prefix for themes variables
+      prefix: 'nextui', // prefix for themes variables
       addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
       layout: {}, // common layout tokens (applied to all themes)
       themes: {
         light: {
           layout: {}, // light theme layout tokens
           colors: {
-            background: "#FEFCF7", // or DEFAULT
-            foreground: "#18181b", // or 50 to 900 DEFAULT// light theme colors
+            background: '#FEFCF7', // or DEFAULT
+            foreground: '#18181b', // or 50 to 900 DEFAULT// light theme colors
+            primary: {
+              50: '#f0fdfa',
+              100: '#ccfbf1',
+              200: '#99f6e4',
+              300: '#5eead4',
+              400: '#2dd4bf',
+              500: '#14b8a6',
+              600: '#0d9488',
+              700: '#0f766e',
+              800: '#115e59',
+              900: '#083344',
+              DEFAULT: '#14b8a6',
+            },
+          },
         },
-      },
         dark: {
           layout: {}, // dark theme layout tokens
-          colors: {}, // dark theme colors
+          colors: {
+            primary: {
+              50: '#f0fdfa',
+              100: '#ccfbf1',
+              200: '#99f6e4',
+              300: '#5eead4',
+              400: '#2dd4bf',
+              500: '#14b8a6',
+              600: '#0d9488',
+              700: '#0f766e',
+              800: '#115e59',
+              900: '#083344',
+              DEFAULT: '#14b8a6',
+            },
+          }, // dark theme colors
         },
-        
       },
     }),
-    require('tailwindcss-animated')
+    require('tailwindcss-animated'),
   ],
-};
-
+}
 
 export default config
