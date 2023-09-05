3ec427503e8d28b2a348db9d0b64dfb724d10081
danilopgon <dani.lopezgonzalez.94@gmail.com>
2023-09-05 18:03:31 +0200

    Remove marquee shadow

--- web/src/app/components/Marquee/Marquee.tsx
+++ web/src/app/components/Marquee/Marquee.tsx
@@ -7,7 +7,7 @@ interface MarqueeProps {
 const Marquee = ({ text }: MarqueeProps): JSX.Element => {
   return (
     <section>
-      <div className="bold relative flex w-full overflow-x-hidden whitespace-nowrap bg-primary uppercase text-primary-foreground shadow-lg  shadow-primary-300">
+      <div className="bold relative flex w-full overflow-x-hidden whitespace-nowrap bg-primary uppercase text-primary-foreground">
         <div className="animate-marquee bg-primary py-14">
           <span className="mx-4 text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl">{text}</span>
           <span className="mx-4 text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl">{text}</span>
