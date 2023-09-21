3ecd4ba63b10acd3cf6a226a0248d3874d7d96c7
danilopgon <dani.lopezgonzalez.94@gmail.com>
2023-09-21 19:13:50 +0200

    fix: honeypot and send email with reset values

--- web/src/app/components/Contact/ContactForm/ContactForm.tsx
+++ web/src/app/components/Contact/ContactForm/ContactForm.tsx
@@ -1,19 +1,33 @@
-'use client'
-
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { Input, Textarea } from '@nextui-org/input'
 import { Button } from '@nextui-org/button'
 
 import EmailInput from '../../../types/EmailInput'
 
+type ContactFormData = EmailInput & {
+  honey: string
+}
+
 const ContactForm = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
-  } = useForm<EmailInput>()
+  } = useForm<ContactFormData>({
+    defaultValues: {
+      name: '',
+      email: '',
+      message: '',
+      honey: '',
+    },
+  })
 
-  const onSubmit: SubmitHandler<EmailInput> = async (data: EmailInput) => {
+  const onSubmit: SubmitHandler<ContactFormData> = async (data: ContactFormData) => {
+    if (data.honey) {
+      return
+    }
+
     try {
       const response = await fetch('/api/sendmail', {
         method: 'POST',
@@ -24,6 +38,7 @@ const ContactForm = () => {
       })
       if (response) {
         console.log(response)
+        reset()
       }
     } catch (error) {
       console.log(error)
@@ -37,9 +52,9 @@ const ContactForm = () => {
           id="name"
           type="text"
           placeholder="Nombre"
+          {...register('name', { required: true, maxLength: 80 })}
           color={errors.name ? 'danger' : 'default'}
           errorMessage={errors.name ? 'Introduce un nombre valido' : null}
-          {...register('name', { required: true, maxLength: 80 })}
         />
       </div>
       <div className="flex flex-col gap-2">
@@ -47,19 +62,23 @@ const ContactForm = () => {
           id="email"
           type="email"
           placeholder="Email"
+          {...register('email', { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })}
           color={errors.email ? 'danger' : 'default'}
           errorMessage={errors.email ? 'Introduce un email valido' : null}
-          {...register('email', { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })}
         />
       </div>
       <div className="flex flex-col gap-2">
         <Textarea
           id="message"
           placeholder="Introduce tu mensaje (hasta 500 caracteres)"
+          {...register('message', { required: true, maxLength: 500 })}
           color={errors.message ? 'danger' : 'default'}
           errorMessage={errors.message ? 'Inroduce tu mensaje' : null}
-          {...register('message', { required: true, maxLength: 500 })}
         />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label className="sr-only">Don’t fill this out if you're human: </label>
+        <input id="honey" type="text" autoComplete="off" className=" -z-10 hidden" {...register('honey')} />
       </div>
       <Button color="primary" type="submit">
         Enviar
